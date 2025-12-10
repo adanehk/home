990752fd1f5d9b10f0247df5b3252ca735bc49a9
Adan El Hnini El Karti <a.elhnini@CEMK.LOCAL>
2025-12-10 12:09:43 +0100

    Actualizar archivos y añadir nuevos

--- objetos/Coche.java
+++ objetos/Coche.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
 
 public class Coche{
+    Scanner entrada = new Scanner(System.in); 
     private String matricula;
     private String modelo;
     private String marca;
-    private String kilometros;
+    private int kilometros;
+    private boolean nuevo = false;
 
 
-public Coche(String matricula,String modelo,String marca,String kilometros){
+public Coche(String matricula,String modelo,String marca,int kilometros){
     this.matricula=matricula;
     this.modelo=modelo;
     this.marca=marca;
@@ -26,9 +29,50 @@ public String getModelo(){
 public String getMarca(){
         return marca;
     }
-public String getKilometros(){
+public int getKilometros(){
         return kilometros;
     }
+public boolean getNuevo(){
+    return nuevo;
+}
+public void setMatricula(String matricula){
+        this.matricula=matricula;
+    }
+public void setModelo(String modelo){
+        this.modelo=modelo;
+    }
+public void setMarca(String marca){
+        this.marca=marca;
+    }
+public void setKilometros(int kilometros){
+        this.kilometros=kilometros;
+    }
+public void setNuevo(boolean nuevo){
+    this.nuevo=nuevo;
+}
 
+public String toString(){
+        return  " matricula: " + matricula +" marca: " + marca + " kilometros: " + kilometros;
+    }
+public void viajar(){
+    System.out.println("¿De cuantos kilometros va a ser el viaje?");
+    int nuevoskilometros = entrada.nextInt();
+    while (nuevoskilometros < 0) { 
+        System.out.println("No puedes viajar 0 o menos kilometros");
+        nuevoskilometros = entrada.nextInt();
+    }
+    System.out.println("Tu coche ha pasado de tener " + kilometros + " a tener " + (kilometros + nuevoskilometros) + " kilometros.");
+    if(kilometros == 0 && nuevoskilometros == 0){
+        nuevo = true;
+    }
+    if(kilometros > 0){
+        nuevo=false;
+    }
+}
+
+
+public void compararKilometros(Coche coche){
+
+}
 
 }
--- objetos/objetosCoche.java
+++ objetos/objetosCoche.java
@@ -1,8 +1,27 @@
 
 public class objetosCoche{
     public static void main(String[] args) {
-        Coche coche1 = new Coche();
-        Coche coche2 = new Coche();
-        Coche coche3 = new Coche();
-    }
+        Coche coche1 = new Coche("123ABC12", "Skyline", "Nissan", 20000 );
+        Coche coche2 = new Coche("321CBA21", "7 Gti", "GOLF", 5000);
+        Coche coche3 = new Coche("434ZSD93", "Urus", "Lamborgini", 0);
+
+        System.out.println(coche1);        
+        System.out.println(coche2);        
+        System.out.println(coche3);       
+        coche1.setMatricula("231FDS23");
+        System.out.println("");
+        System.out.println(coche1);
+        
+         coche2.setMatricula("434ZSD93");
+         System.out.println("");
+        System.out.println(coche2);    
+        coche3.setMatricula("123ABC12");
+        System.out.println("");
+        System.out.println(coche3); 
+        coche1.viajar();   
+        coche1 = nuevo;
+        coche2 = nuevo;
+        
+        
+        }
 }
